1e12e5bfcd3f777d3118aab6bcb10fce9be5689e
Abhinav <abhinavjha98ald@gmail.com>
2019-10-09 23:34:08 +0530

    Added the features

--- src/app/rental/rental.component.ts
+++ src/app/rental/rental.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental.component.scss']
 })
 export class RentalComponent implements OnInit {
-rentals: any[] = [1,2,3,4];
+
   constructor() { }
 
   ngOnInit() {
